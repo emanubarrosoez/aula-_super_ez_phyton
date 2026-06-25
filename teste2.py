a4c845f57a0c035222093e7357922ef2e74173cd
emanubarrosoez <emanubarroso6@gmail.com>
2026-06-24 21:32:05 -0300

    Create teste2.py
     Questão 5) Apresentação de telejornais

---
+++ teste2.py
@@ -0,0 +1,16 @@
+sobrenome = input("Digite o sobrenome do(a) apresentador(a): ")
+
+if sobrenome == "Zé PINHEIRO":
+    print("Telejornal: Bom Dia Nação")
+
+elif sobrenome == "Ana Carla ARAÚJO":
+    print("Telejornal: Bom Dia Nação")
+
+elif sobrenome == "Bill BONNER":
+    print("Telejornal: Jornal Brasileiro")
+
+elif sobrenome == "CARLA VASCONCELOS":
+    print("Telejornal: Jornal Brasileiro")
+
+else:
+    print("Apresentador(a) desconhecido(a).")
